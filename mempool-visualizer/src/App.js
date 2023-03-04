c5a77a8d4142f93732a174ba3bde35b46b941f78
Jevan Malaviya <jevan.malaviya@gmail.com>
2023-03-04 10:24:57 -0500

    add mined transactions, limited watching scope to a signel contract, changed frontend layout to columns

---
+++ mempool-visualizer/src/App.js
@@ -0,0 +1,89 @@
+import './App.css';
+import { Alchemy, AlchemySubscription } from "alchemy-sdk";
+import { useEffect, useState } from 'react';
+
+const alchemy = new Alchemy({
+  apiKey: 'sp8CRkb8K-I8By11GTiCMuuoteqGNfNY'
+});
+
+function App() {
+  const [pendingTxList, setPendingTxList] = useState([]);
+  const [minedTxList, setMinedTxList] = useState([]);
+  useEffect(() => {
+    console.log("listening");
+    alchemy.ws.on({
+      method: AlchemySubscription.PENDING_TRANSACTIONS,
+      toAddress: '0xEf1c6E67703c7BD7107eed8303Fbe6EC2554BF6B',
+      fromAddress: '0xEf1c6E67703c7BD7107eed8303Fbe6EC2554BF6B',
+      hashesOnly: false,
+    }
+      ,
+      (tx) => {
+      console.log(tx);
+      setPendingTxList(prev => [...prev, tx.hash]);
+      });
+
+    alchemy.ws.on({
+      method: AlchemySubscription.MINED_TRANSACTIONS,
+      addresses: [
+        {from: '0xEf1c6E67703c7BD7107eed8303Fbe6EC2554BF6B'},
+        {to: '0xEf1c6E67703c7BD7107eed8303Fbe6EC2554BF6B'}
+      ],
+      hashesOnly: false,
+    }
+      ,
+      (tx) => {
+      console.log(tx);
+      setMinedTxList(prev => [...prev, tx.transaction.hash]);
+      })
+
+      return () => {
+        console.log("Stopped listening");
+        stopTx();
+      }
+    },[]);
+
+  function stopTx() {
+  alchemy.ws.removeAllListeners();
+}
+
+
+  return (
+    <div className="App">
+      <h1>Watching Uniswap at: {'0xEf1c6E67703c7BD7107eed8303Fbe6EC2554BF6B'}</h1>
+      <button onClick={stopTx}>Ermagerd Stahp</button>
+      <div className='transactions-wrapper'>
+        <div className='pending'>
+          <h1>Pending Transactions</h1>
+          <ul>
+          {
+            pendingTxList.map((tx) => {
+              return (
+                <li key={tx}>
+                <p>{tx}</p>
+              </li>
+              )
+            })
+          }
+          </ul>
+        </div>
+        <div className='mined'>
+          <h1>Mined Transactions</h1>
+          <ul>
+          {
+            minedTxList.map((tx) => {
+              return (
+                <li key={tx}>
+                <p>{tx}</p>
+              </li>
+              )
+            })
+          }
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
